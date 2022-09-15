dcf09764476e2fb9e3938e56cff4cdc327e936fa
nknguyenhc <nknguyentdn@gmail.com>
2022-09-15 21:41:01 +0800

    added 3 first skills
    no implementation of the skills yet

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,17 @@
 extends KinematicBody2D
 
 
+# movement
 var velocity = Vector2.ZERO
+const JUMP_STRENGTH = 100
+
+# skills
+const Punch = preload("res://Scenes/punch.tscn")
+var punch
+const Kick = preload("res://Scenes/kick.tscn")
+var kick
+const Boomerang = preload("res://Scenes/boomerang.tscn")
+var boomerang
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,5 +23,30 @@ func _ready():
 #func _process(delta):
 #	pass
 
+
+func player_punch():
+	punch = Punch.instance()
+
+
+func player_kick():
+	kick = Kick.instance()
+
+
+func player_boomerang():
+	boomerang = Boomerang.instance()
+
+
 func _physics_process(delta):
+	# movement
+	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
+		velocity.y = -JUMP_STRENGTH
+	velocity = move_and_slide(velocity, Vector2.UP)
 	
+	# activate skills
+	if Input.is_action_just_pressed("ui_skill1"):
+		player_punch()
+	if Input.is_action_just_pressed("ui_skill2"):
+		player_kick()
+	if Input.is_action_just_pressed("ui_skill3"):
+		player_boomerang()
