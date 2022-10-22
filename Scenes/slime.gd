21aafdd7564a5283087fd3b66aaf992c51fd4b3c
0-yibai <110611841+0-yibai@users.noreply.github.com>
2022-10-22 14:48:41 +0800

    Update slime.gd

--- Scenes/slime.gd
+++ Scenes/slime.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 
 # Declare member variables here. Examples:
 var gravity = 2
-var velocity = Vector2.ZERO
+var velocity = Vector2(0, -20)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,5 +13,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	
-	
+	if is_on_floor():
+		velocity = Vector2(0, -20)
+	velocity.y += gravity
+	position += velocity
