d644fb1cd1016dbd2a72159a069a77208ac1f2af
nknguyenhc <nknguyentdn@gmail.com>
2022-10-22 23:05:27 +0800

    Spit Fire Created
    Created Spit Fire skill for dragon, not finished, at line 76

--- Scripts/fire_by_dragon.gd
+++ Scripts/fire_by_dragon.gd
@@ -1,19 +1,28 @@
-extends KinematicBody2D
+extends Area2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+const SCALE = 4
+
 var velocity
-var speed = 5
-var ground_height  # y-position of platform
-var platform_burning = preload("res://Scenes/platform_burning.tscn")
-var initial_position   # the x-position where fireball just hit the ground
-var initialised = false  # will be set to true once the fireball hits ground
-var platform   # instances of platform_burning
-var fire_spreading = true
-const LENGTH_PER_PLATFORM = 3
-const HURT_DEGREE = 1
+const SPEED = 2
 
+# during flight
+var direction # only used if the fire ball is travelling in mid air
+var landed # will be set to true once the fireball hits ground
+const marker = -950
+
+# during spreading
+var next_fire_ball
+var next_fire_ball2
+var direction_of_spread # both or left or right
+const SPACE_IN_BETWEEN = 10
+const WAIT_TIME = 1
+var fire_spread = false # set to true when two fire balls next to it have appeared
+var rotate_direction = false # set to true if the side hits a wall
+var test_list
+
+var player
+var dragon
 
 
 #start 2 timers
@@ -26,37 +35,70 @@ const HURT_DEGREE = 1
 
 
 func _ready():
-	velocity = (get_parent().get_node("player").position - position).normalized() * speed
-	#get y-position of platform(ground)
-	ground_height = get_parent().get_node("ground").position.x
-
+	if direction_of_spread == "both":
+		scale.x = SCALE
+		scale.y = SCALE
+	if not landed:
+		direction = player.position - position
+		velocity = Vector2.ZERO
+		velocity.x = direction.x / sqrt(pow(direction.x, 2) + pow(direction.y, 2)) * SPEED
+		velocity.y = direction.y / sqrt(pow(direction.x, 2) + pow(direction.y, 2)) * SPEED
+	if rotate_direction:
+		print("rotate direction true!")
 
 
 func _physics_process(delta):
-	if (fire_spreading == true):
+	if not landed:
+		if position.y > marker:
+			get_node("Bottom").set_collision_mask_bit(2, true)
 		position += velocity
-	
-		#when the fireball hits the ground (equal y position)
-		if position.y == ground_height: # change this to body-entered  # We do this comparison instead of using body_entered because now the position of platfom can be adjusted more accuractely
-			velocity.y = 0  #make it move horizontally in the same initial horizontal direction
-			if (initialised == false):
-				initial_position = position.x  # the x-position where fireball just hit the ground
-				initialised = true
-		if (position.x - initial_position) % LENGTH_PER_PLATFORM == 0:
-			platform = platform_burning.instance()  #create instances of static body of burnt ground along the direction, one frame per unit distance
-			platform.position = position
-			add_child(platform)
+	else:
+		if not fire_spread:
+			next_fire_ball = dragon.FireBall.instance()
+			next_fire_ball.player = player
+			next_fire_ball.dragon = dragon
+			next_fire_ball.landed = true
+			if direction_of_spread == 'both':
+				next_fire_ball.direction_of_spread = "right"
+				next_fire_ball.position.x = SPACE_IN_BETWEEN
+				next_fire_ball2 = dragon.FireBall.instance()
+				next_fire_ball2.player = player
+				next_fire_ball2.dragon = dragon
+				next_fire_ball2.landed = true
+				next_fire_ball2.direction_of_spread = "left"
+				next_fire_ball2.position.x = -SPACE_IN_BETWEEN
+			else:
+				next_fire_ball.direction_of_spread = direction_of_spread
+				if direction_of_spread == "left":
+					next_fire_ball.position.x = -SPACE_IN_BETWEEN
+					test_list = get_node("Left").get_overlapping_bodies()
+					if len(test_list) > 0 and (test_list[0].name == "TileMap" or test_list[0].name == "TileMap2"):
+						rotate_direction = true
+				else:
+					next_fire_ball.position.x = SPACE_IN_BETWEEN
+			fire_spread = true
+			get_node("WaitTimer").start(WAIT_TIME)
 
 
-func _on_burn_time_timeout():
-	fire_spreading = false
+func _on_Left_body_entered(body):
+	if body.name == "TileMap" or body.name == 'TileMap2':
+		if direction_of_spread == 'left' or direction_of_spread == 'both':
+			rotate_direction = true
 
 
-func _on_fire_box_body_entered(body):
-	if body.name == "player":
-		body.health -= HURT_DEGREE
-	#if body.name == "map1":
-		#burn the ground to a certain distance
+func _on_Right_body_entered(body):
+	if body.name == "TileMap" or body.name == 'TileMap2':
+		if direction_of_spread == 'right' or direction_of_spread == 'both':
+			rotate_direction = true
 
-func _on_hurt_stay_time_timeout():
-	queue_free()
+
+func _on_WaitTimer_timeout():
+	add_child(next_fire_ball)
+	if direction_of_spread == 'both':
+		add_child(next_fire_ball2)
+
+
+func _on_Bottom_body_entered(body):
+	if body.name == "TileMap" or body.name == "TileMap2":
+		landed = true
+		print("landed")
