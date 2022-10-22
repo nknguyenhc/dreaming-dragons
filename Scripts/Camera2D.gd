a6e4266f3de3abd11831acecab775c5842f2336e
0-yibai <110611841+0-yibai@users.noreply.github.com>
2022-10-22 14:10:50 +0800

    Update Camera2D.gd

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -13,12 +13,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if get_parent().get_parent().get_node("Player").position.x - position.x > 900:
-		global_position.x = get_parent().get_parent().get_node("Player").global_position.x - 900
-	if get_parent().get_parent().get_node("Player").position.x - position.x < -900:
-		global_position.x = get_parent().get_parent().get_node("Player").global_position.x + 900
-	if get_parent().get_parent().get_node("Player").position.y - position.y > 400:
-		global_position.y = get_parent().get_parent().get_node("Player").global_position.y - 400
-	if get_parent().get_parent().get_node("Player").position.y - position.y < -400:
-		global_position.y = get_parent().get_parent().get_node("Player").global_position.y + 400
+	if get_parent().get_parent().get_node("Player").is_boss_fight_started == false:
+		if get_parent().get_parent().get_node("Player").position.x - position.x > 900:
+			global_position.x = get_parent().get_parent().get_node("Player").global_position.x - 900
+		if get_parent().get_parent().get_node("Player").position.x - position.x < -900:
+			global_position.x = get_parent().get_parent().get_node("Player").global_position.x + 900
+		if get_parent().get_parent().get_node("Player").position.y - position.y > 400:
+			global_position.y = get_parent().get_parent().get_node("Player").global_position.y - 400
+		if get_parent().get_parent().get_node("Player").position.y - position.y < -400:
+			global_position.y = get_parent().get_parent().get_node("Player").global_position.y + 400	
+	else:
+		position = Vector2(3700, -1280)
+		scale.x = 1.5
+		scale.y = 1.5
+	
 
