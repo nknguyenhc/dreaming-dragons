a879a2e06302b0b8790de12a6893e106117f78cc
4F5DA2 <E0969244@u.nus.edu>
2022-10-31 01:44:38 +0800

    Medicine adjustments

--- Scripts/medicine.gd
+++ Scripts/medicine.gd
@@ -1,18 +1,19 @@
 extends StaticBody2D
 
-func _process(delta):
-	pass
+var is_used = false
 
 
 func _on_RecoverBox_body_entered(body):
-	if body.name == "Player":
-		get_node("AnimatedSprite").animation = "open"
+	if body.name == "Player" and not is_used:
+		is_used = true
+		if get_parent().get_node("Player").health < get_parent().get_node("Player").MAX_HEALTH / 5 * 4:
+			get_parent().get_node("Player").health += get_parent().get_node("Player").MAX_HEALTH / 5
+		else:
+			get_parent().get_node("Player").health = get_parent().get_node("Player").MAX_HEALTH
+			get_node("AnimatedSprite").animation = "open"
+		get_node("heal").play()	
 		get_node("timer").start()
 
 
 func _on_timer_timeout():
-	if get_parent().get_node("Player").health < get_parent().get_node("Player").MAX_HEALTH / 4 * 3:
-		get_parent().get_node("Player").health += get_parent().get_node("Player").MAX_HEALTH / 4
-	else:
-		get_parent().get_node("Player").health = get_parent().get_node("Player").MAX_HEALTH
 	queue_free()
