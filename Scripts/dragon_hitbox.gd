fb5913b1ebc93a51296a7495dcc0b09a7fcfa0b3
4F5DA2 <E0969244@u.nus.edu>
2022-11-02 02:36:27 +0800

    balance

--- Scripts/dragon_hitbox.gd
+++ Scripts/dragon_hitbox.gd
@@ -12,14 +12,16 @@ func _ready():
 
 
 func _physics_process(delta):
-	if dragon.current_state == dragon.BOSS_STATE.DASH:
-		damage = 10
-	elif dragon.current_state == dragon.BOSS_STATE.KICK:
+	if dragon.is_death_initiated:
+		damage = 0
+	elif dragon.current_state == dragon.BOSS_STATE.DASH:
 		damage = 15
-	elif dragon.stomp_initiated and dragon.delay_timeout:
+	elif dragon.current_state == dragon.BOSS_STATE.KICK:
 		damage = 20
+	elif dragon.stomp_initiated and dragon.delay_timeout:
+		damage = 25
 	else:
-		damage = 4
+		damage = 6
 
 
 func _on_Hitbox_body_entered(body):
